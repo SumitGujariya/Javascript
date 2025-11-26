719504b0d78a86fa6894af6e289a6a7c26fae1b4
SumitGujariya <gujariyasumit96@gmail.com>
2025-11-27 01:28:58 +0530

    push in my repo

--- JavaScript--/Basics.1/1Variables.js
+++ JavaScript--/Basics.1/1Variables.js
@@ -1,8 +1,8 @@
-const accountId = 3243
-const accountId2 = 43 // you can change but you need to give a deiffrent nad unique name to the variable 
-let accountEmail = "sumit@google.com" // this will change the value with the same name and variable (overwrite the value)
-var accountPassword = "22222"
-accountCity = "jaipur"
+const accountId = 3243 // You can not change after declare the value in it you want to declare the unique value and id for the const variable.
+const accountId2 = 43 // You can change but you need to give a deiffrent Id and unique name to the variable.
+let accountEmail = "sumit@google.com" // This will change the value with the same name and variable (overwrite the value).
+var accountPassword = "22222" // We not you in that time so its not necessary to learn we use let & const most of the time. (Read line no 20.)
+accountCity = "jaipur" // You can aslo declare the value without any variables, but its not good way to define the value.
 let accountState;
 
 
--- JavaScript--/Basics.1/3conversion_operation.js
+++ JavaScript--/Basics.1/3conversion_operation.js
@@ -1,9 +1,12 @@
+// Conversion in JavaScript 
+// We write the any conversion method with capital of the conversion we want.
+
 let score = "33abc"
 
 // console.log(typeof score); // you can write the score wtih the help of both method the first one 
 // console.log(typeof (score)); // second one in brecets
 
-let valueInNumber = Number(score)
+let valueInNumber = Number(score) // Conversion into number value
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
@@ -12,15 +15,17 @@ let valueInNumber = Number(score)
 //  true => 1; false => 0
 
 let isLoggedIn = ""
-let boleanIdLoggedIn = Boolean(isLoggedIn)
+let boleanIdLoggedIn = Boolean(isLoggedIn) // conversion into boolean value
 // console.log(boleanIdLoggedIn);
 
+
+// This are the example of some conversion.
 // 1 => true; 0 => false
 // "" => false
 // "sumit" => true
 
 let stringInNumber = 22
-let srtingIsNumber = String(stringInNumber)
+let srtingIsNumber = String(stringInNumber) // converison into string value.
 // console.log(srtingIsNumber);
 // console.log(typeof srtingIsNumber);
 
@@ -30,14 +35,16 @@ let srtingIsNumber = String(stringInNumber)
 
 let value = 5
 let negValue = -value
-// console.log(negValue);
+// console.log(negValue); // THe output is = -5
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3);
+
+// This all are method of operation in javascript.
+// console.log(2+2); // Addition
+// console.log(2-2); // Substraction
+// console.log(2*2);  // Multiplication
+// console.log(2**3); // Power 
+// console.log(2/3); // Devide 
+// console.log(2%3); // Modulo
 
 
 let str1 = "hello"
@@ -45,10 +52,10 @@ let str2 = " sumit"
 let str3 = str1 + str2
 // console.log(str3)
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2); // The output is = 12
+// console.log(1 + "2"); // The output is = 12
+// console.log("1" + 2 + 2); // if string comes first they treat all like string. the output is = 122
+// console.log(1 + 2 + "2"); // if string comes in last so they complete the conversion first. the output is = 32
 
 // console.log( (3 + 4) * 5 % 3);
 
